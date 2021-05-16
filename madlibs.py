4bd91664eef1e1544a687dd0d610dae0fa93e3d1
Victor Umunna <umunnavictor01@gmail.com>
2021-05-16 21:50:48 +0100

    add madlib project

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# string concatenation (aka how to put strings together)
+
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+famous_person = input('Famous Person: ')
+
+madlib = f"Computer progrmming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
